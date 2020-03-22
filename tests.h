21ca3a2c7134d3433900d11e4da9722eb7c1e3cd
cardcaptr <55108580+cardcaptr@users.noreply.github.com>
2020-03-22 16:54:56 -0400

    Create tests.h

---
+++ tests.h
@@ -0,0 +1,17 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+
+class Tests
+{
+public:
+    Tests();
+
+
+};
+
+void testSpringParticles();
+void testEulerIntegration();
+void testVerletIntegration();
+
+#endif // TESTS_H
