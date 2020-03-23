8d5cfb73ac7cbe576cc838c7e456187617d82c89
cardcaptr <55108580+cardcaptr@users.noreply.github.com>
2020-03-22 22:14:57 -0400

    Update plotMassFlow.py

--- plotMassFlow.py
+++ plotMassFlow.py
@@ -29,6 +29,25 @@ print("Total mass is : ", np.max(particlesLeft) * mass)
 plt.plot(t,particlesLeft * mass, label='Experimental data') 
 plt.plot(t[i0:i1],linearfit * mass, label='Linear Regression') 
 
+#%% smol hole 0.2
+t,particlesLeft=np.loadtxt("C:/Users/Ad/Desktop/particles/smallhole500/particleCount.dat",skiprows=1, unpack=True)
+       
+i0=250 #start fit here
+i1=400 #end fit here
+polynom1=np.polyfit(t[i0:i1],particlesLeft[i0:i1],1)
+linearfit=polynom1[0]*t[i0:i1]+polynom1[1]
+
+print("Mass flow for 500 particles : %3.3f (kg/s)" % (-polynom1[0] * mass))
+print("Total mass is : ", np.max(particlesLeft) * mass)
+
+plt.plot(t,particlesLeft * mass, label='Experimental data-s500') 
+plt.plot(t[i0:i1],linearfit * mass, label='Linear Regression-s500')
+
+
+plt.legend()
+plt.show
+
+
 #%% 100 particles 0.4m hole
 t,particlesLeft=np.loadtxt("C:/Users/Ad/Desktop/particles/100p/particleCount.dat",skiprows=1, unpack=True)
        
